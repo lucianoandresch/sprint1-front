6b8287ede6a204438e38110cd23287309defdf28
LucasCasanueva <lucas.casanueva@uc.cl>
2022-11-16 01:13:52 -0300

    feat(DriverRegisterRequests):style of view Drivers Register Requests

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 import Register from './components/Register';
+import DriverRegisterRequests from './components/DriverRegisterRequests';
 import { BrowserRouter as Router1, Route, Routes } from 'react-router-dom';
 
 export default function RoutesApp() {
@@ -12,6 +13,7 @@ export default function RoutesApp() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/DriverRegisterRequests" element={<DriverRegisterRequests />} />
         </Routes>
       </Router1>
     </div>
