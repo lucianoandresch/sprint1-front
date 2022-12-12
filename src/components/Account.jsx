8b47db041684767b6bccf8eb0aaec48e924c17e5
luciano1 <you@example.com>
2022-12-12 00:31:48 -0300

    perfiles

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import loginImage from '../assets/login_car.png';
+import React, { useEffect, useState } from 'react';
+import Collapsible from 'react-collapsible';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import useAuth from '../hooks/useAuth';
+import '../styles/requests.css';
 import { Navbar } from './widgets/Navbar';
-// import { useParams } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
 
-export default function CategoryDetail() {
-  // const {id} = useParams();
-  // const [lesson, setLesson] = useState(id);
+export default function RequestOwner() {
+  const { currentUser } = useAuth();
+  const [requestz, setRequests] = useState([]);
+  console.log(currentUser.userId);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: currentUser?.token,
+      },
+    };
+
+    try {
+      fetch(`${process.env.REACT_APP_API_URL}users/${currentUser.userId}`, requestOptions)
+        .then((response) => {
+          if (response.status !== 200) {
+            return [];
+          }
+          return response.json();
+        })
+        .then((json) => {
+          console.log('JSON', json);
+          console.log('REQUESTS', requestz);
+          setRequests(json.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="login-view">
-        <h2 className="login-title">Mi Perfil</h2>
+      <div className="view-container">
+        <h1 className="title">Mi Perfil</h1>
+        <h5>Primer Nombre: {requestz.firstName}</h5>
+        <h5>Apellido: {requestz.lastName}</h5>
+        <h5>Antecedentes: {requestz.background}</h5>
+        <h5>Email: {requestz.email}</h5>
+        <h5>Rating: {requestz.rating}</h5>
       </div>
-    </div>
+      <div className="empty-div-footer"></div>
+    </>
   );
 }
